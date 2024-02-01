b8a20905d01c8d068bdf0c5141ee553c4140788e
Warren Ste Wu <wuwarren7360@gmail.com>
2024-02-01 00:04:32 -0600

    Formed connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
+
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Could not connect to MongoDB', err));
 
 const app = express();
 const port = 3000;
